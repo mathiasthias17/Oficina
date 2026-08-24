8afdbf15b0edab5f6d4cea6b8021f91731dca0d3
LabInfo <LabInfo@TERREOA4M17>
2026-08-24 12:24:19 -0300

    Adicionar arquivos de projeto.

---
+++ Oficina/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Oficina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.peçasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.finanaceiroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fornecedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.funcionáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.veiculosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.peçasToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.serviçosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.veículosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.informativosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Orange;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.peçasToolStripMenuItem,
+            this.clientesToolStripMenuItem,
+            this.finanaceiroToolStripMenuItem,
+            this.fornecedoresToolStripMenuItem,
+            this.funcionáriosToolStripMenuItem,
+            this.veiculosToolStripMenuItem,
+            this.informativosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1194, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "Financeiro";
+            // 
+            // peçasToolStripMenuItem
+            // 
+            this.peçasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.veículosToolStripMenuItem});
+            this.peçasToolStripMenuItem.Name = "peçasToolStripMenuItem";
+            this.peçasToolStripMenuItem.Size = new System.Drawing.Size(89, 29);
+            this.peçasToolStripMenuItem.Text = "Clientes";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(92, 29);
+            this.clientesToolStripMenuItem.Text = "Estoque";
+            // 
+            // finanaceiroToolStripMenuItem
+            // 
+            this.finanaceiroToolStripMenuItem.Name = "finanaceiroToolStripMenuItem";
+            this.finanaceiroToolStripMenuItem.Size = new System.Drawing.Size(117, 29);
+            this.finanaceiroToolStripMenuItem.Text = "Finanaceiro";
+            // 
+            // fornecedoresToolStripMenuItem
+            // 
+            this.fornecedoresToolStripMenuItem.Name = "fornecedoresToolStripMenuItem";
+            this.fornecedoresToolStripMenuItem.Size = new System.Drawing.Size(135, 29);
+            this.fornecedoresToolStripMenuItem.Text = "Fornecedores";
+            // 
+            // funcionáriosToolStripMenuItem
+            // 
+            this.funcionáriosToolStripMenuItem.Name = "funcionáriosToolStripMenuItem";
+            this.funcionáriosToolStripMenuItem.Size = new System.Drawing.Size(128, 29);
+            this.funcionáriosToolStripMenuItem.Text = "Funcionários";
+            // 
+            // veiculosToolStripMenuItem
+            // 
+            this.veiculosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.peçasToolStripMenuItem1,
+            this.serviçosToolStripMenuItem});
+            this.veiculosToolStripMenuItem.Name = "veiculosToolStripMenuItem";
+            this.veiculosToolStripMenuItem.Size = new System.Drawing.Size(177, 29);
+            this.veiculosToolStripMenuItem.Text = "Ordem de serviços";
+            // 
+            // peçasToolStripMenuItem1
+            // 
+            this.peçasToolStripMenuItem1.Name = "peçasToolStripMenuItem1";
+            this.peçasToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.peçasToolStripMenuItem1.Text = "Peças";
+            // 
+            // serviçosToolStripMenuItem
+            // 
+            this.serviçosToolStripMenuItem.Name = "serviçosToolStripMenuItem";
+            this.serviçosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.serviçosToolStripMenuItem.Text = "Serviços";
+            // 
+            // veículosToolStripMenuItem
+            // 
+            this.veículosToolStripMenuItem.Name = "veículosToolStripMenuItem";
+            this.veículosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.veículosToolStripMenuItem.Text = "Veículos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(328, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(518, 129);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Oficina Thomas Turbo";
+            // 
+            // informativosToolStripMenuItem
+            // 
+            this.informativosToolStripMenuItem.Name = "informativosToolStripMenuItem";
+            this.informativosToolStripMenuItem.Size = new System.Drawing.Size(129, 29);
+            this.informativosToolStripMenuItem.Text = "Informativos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1194, 617);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Orange;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem peçasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem finanaceiroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fornecedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem funcionáriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem veiculosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem peçasToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem serviçosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem veículosToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem informativosToolStripMenuItem;
+    }
+}
+
